18ab126bc95b5564b01d2ff1b77071394893bbaf
georgvi <ifreyd@mail.ru>
2022-08-05 22:05:22 +0300

    задачи 5 и 7

---
+++ Zadacha5/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввыдите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int num2 = -num;
+
+Console.WriteLine("Результат: ");
+
+if (num<0) {
+    while (num<=num2){
+        Console.WriteLine(num);
+        num++;
+
+    }
+}
+else {
+     while (num2<=num) {
+        Console.WriteLine(num2);
+        num2++;
+
+    }
+}
---
+++ Zanacha7/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+int result = num%10;
+Console.WriteLine("Последняя цифра числа " + result);
+
+
+
