cb5e54856acb2c33566dd278ca5b80a94b0fef8c
Georgyvi <ifreyd@mail.ru>
2022-08-08 15:59:07 +0300

    Задача два из ДР

---
+++ ZadachaSem2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Б");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число В");
+int numberC = Convert.ToInt32(Console.ReadLine());
+
+//Определяем большее число с выводом на консоль
+if (numberA > numberB)
+{
+    if (numberA > numberC){
+        Console.WriteLine("Число MAX -> " + numberA);
+    }
+    else {
+        Console.WriteLine("Число MAX -> " + numberC);
+    }
+    }
+    else 
+{
+    if (numberB > numberC){
+        Console.WriteLine("Число MAX -> " + numberB);
+    }
+    else {
+        Console.WriteLine("Число MAX -> " + numberC);
+    }
+    }
+
+
+if (numberA == numberB)
+{
+    if (numberA == numberC)
+{
+    Console.WriteLine("Числа равны ");
+}
+    
+}
