60360e2b0c4831a10a4e18a9d1a06a9c3ceee698
georgvi <ifreyd@mail.ru>
2022-08-08 22:58:36 +0300

    Задачи 34

---
+++ ZadachaSem3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Проверка на четность
+int result = number % 2;
+
+if (result == 0)
+{
+    Console.WriteLine("Введено четное число");
+}
+else
+    Console.WriteLine("Введено нечетное число");
---
+++ ZadachaSem4/Program.cs
@@ -0,0 +1,16 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int numberStart = 2;
+
+//Цикл вывода четных чисел от 0 до N
+
+Console.WriteLine("Четные чисела от 0 до N: ");
+
+while (numberStart < numberN)
+{
+        Console.WriteLine(numberStart);
+    numberStart = numberStart + 2;
+}
+
