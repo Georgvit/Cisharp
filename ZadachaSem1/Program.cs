54c39335291533dea5b1cd2c3e2a9bd60be1e214
Georgyvi <ifreyd@mail.ru>
2022-08-08 15:33:18 +0300

    Первая задача из ДР

---
+++ ZadachaSem1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.WriteLine("Введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число Б");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+//Определяем большее число с выводом на консоль
+if (numberA > numberB)
+{
+    Console.WriteLine("Число MAX -> " + numberA);
+    Console.WriteLine("Число MIN -> " + numberB);
+}
+
+if (numberA < numberB)
+{
+    Console.WriteLine("Число MAX -> " + numberB);
+    Console.WriteLine("Число MIN -> " + numberA);
+}
+
+if (numberA == numberB)
+{
+    Console.WriteLine("Числа равны ");
+}
