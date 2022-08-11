3756eaa920d6ca4e9e12161fea03591c8069048d
georgvi <ifreyd@mail.ru>
2022-08-11 23:14:55 +0300

    домашка второй семинар

---
+++ ZadachaDZ10/Program.cs
@@ -0,0 +1,10 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//Вычисляем вторую цифру трехзначного числа
+
+int result = number / 10 % 10;
+Console.WriteLine(result);
---
+++ ZadachaDZ13/Program.cs
@@ -0,0 +1,18 @@
+﻿//Пользовательский ввод чисела и конвертация в тип int
+Console.Clear();
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int ostatok = 10;
+
+//Вычисляем вторую цифру трехзначного числа
+if (ostatok*10 < number){
+
+}
+else
+Console.WriteLine("Нет третьего числа");
+
+
+//int result = number / 10 % 10;
+//Console.WriteLine(result);
