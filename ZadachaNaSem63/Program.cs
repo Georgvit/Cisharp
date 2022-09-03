d2a82bf02905331703ed6886a2249ce9ffdd7cbe
Georgvit <ifreyd@mail.ru>
2022-09-03 18:54:01 +0300

    Семинар 9 ч1

---
+++ ZadachaNaSem63/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Введите число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число N");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+int Natural (int numberOne, int numberTwo){
+
+        if (numberTwo <= numberOne){
+        
+        Console.Write($" {numberTwo} ");
+        
+       numberTwo++;
+    } else {Environment.Exit(0);}
+    return numberTwo = numberTwo + Natural(numberOne, numberTwo);
+    
+    
+}
+
+Natural(numberN, numberM );
